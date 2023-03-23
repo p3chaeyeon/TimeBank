5b5fb90d0b3a59338987bef0e13056e233321958
BbingDdang <39540588+BbingDdang@users.noreply.github.com>
2023-03-23 14:33:28 +0900

    Inquiry 엔티티 작성
    Inquiry 엔티티 작성

---
+++ backend/timebank/src/main/kotlin/kookmin/software/capstone2023/timebank/domain/model/Inquiry.kt
@@ -0,0 +1,40 @@
+package kookmin.software.capstone2023.timebank.domain.model
+
+import java.time.LocalDateTime
+import jakarta.persistence.*
+
+@Entity
+@Table(name = "inquiries")
+class Inquiry(
+
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        val inquiryid: Long = 0,
+
+        @Column(nullable = false)
+        val title: String,
+
+        @Column(nullable = false, columnDefinition = "TEXT")
+        val content: String,
+
+        @Column(columnDefinition = "TEXT")
+        var replyContent: String? = null,
+
+        @Column(nullable = false)
+        val inquiryDate: LocalDateTime = LocalDateTime.now(),
+
+        var replyDate: LocalDateTime? = null,
+
+        @Enumerated(EnumType.STRING)
+        @Column(nullable = false)
+        var replyStatus: InquiryStatus = InquiryStatus.PENDING,
+
+        @ManyToOne(fetch = FetchType.LAZY)
+        @JoinColumn(name = "user_id", nullable = false)
+        val user: User
+)
+
+enum class InquiryStatus {
+    PENDING,
+    ANSWERED
+}
