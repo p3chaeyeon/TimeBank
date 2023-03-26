daeb48000a6aa0d77e498060e0cc5557ab1c53ef
BbingDdang <39540588+BbingDdang@users.noreply.github.com>
2023-03-26 18:18:05 +0900

    Comment Entity 생성

---
+++ backend/timebank/src/main/kotlin/kookmin/software/capstone2023/timebank/domain/model/Comment.kt
@@ -0,0 +1,35 @@
+package kookmin.software.capstone2023.timebank.domain.model
+
+import java.time.LocalDateTime
+import jakarta.persistence.*
+@Entity
+@Table(name = "comments")
+data class Comment(
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        val id: Long = 0,
+
+        @Column(nullable = false, columnDefinition = "TEXT")
+        val content: String,
+
+        @Column(nullable = false)
+        val createdAt: LocalDateTime = LocalDateTime.now(),
+
+        @Enumerated(EnumType.STRING)
+        @Column(nullable = false)
+        var replyStatus: InquiryStatus = InquiryStatus.PENDING,
+
+        @ManyToOne(fetch = FetchType.LAZY)
+        @JoinColumn(name = "inquiry_id", nullable = false)
+        val inquiry: Inquiry,
+
+        @ManyToOne(fetch = FetchType.LAZY)
+        @JoinColumn(name = "user_id", nullable = false)
+        val user: User
+){
+    @Column(name = "user_id", insertable = false, updatable = false)
+    val userId: Long = user.id
+
+    @Column(name = "inquiry_id", insertable = false, updatable = false)
+    val inquiryId : Long = inquiry.id
+}
