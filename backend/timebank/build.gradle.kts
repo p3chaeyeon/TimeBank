15217eea3912db3f8afc1c9a58f9b98751e37004
Jake <jake@daangn.com>
2023-03-31 04:37:06 +0900

    Actuator 추가

--- backend/timebank/build.gradle.kts
+++ backend/timebank/build.gradle.kts
@@ -41,6 +41,8 @@ dependencies {
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 
     implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
+
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-validation")
 
