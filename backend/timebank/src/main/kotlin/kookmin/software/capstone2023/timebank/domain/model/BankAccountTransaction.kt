9ecacee83714318420ae194a2fd038ca4b98e751
jeonghi <jpark0902@kookmin.ac.kr>
2023-03-31 17:40:54 +0900

    [Add] 은행 계좌 거래 엔티티 추가

---
+++ backend/timebank/src/main/kotlin/kookmin/software/capstone2023/timebank/domain/model/BankAccountTransaction.kt
@@ -0,0 +1,30 @@
+package kookmin.software.capstone2023.timebank.domain.model
+import jakarta.persistence.*
+import java.time.LocalDateTime
+
+@Entity
+@Table(name = "BankAccountTransaction")
+data class BankAccountTransaction(
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long = 0,
+
+    val bankAccountId: Long,
+
+    @Enumerated(EnumType.STRING)
+    val code: TransactionCode? = null,
+
+    val amount: Int? = null,
+
+    @Enumerated(EnumType.STRING)
+    var status: TransactionStatus = TransactionStatus.REQUESTED,
+
+    val receiverAccountNumber: String? = null,
+
+    val senderAccountNumber: String? = null,
+
+    val balanceSnapshot: Int? = null,
+
+    @Column(nullable = false)
+    val transactionAt: LocalDateTime = LocalDateTime.now(),
+):BaseTimeEntity()
