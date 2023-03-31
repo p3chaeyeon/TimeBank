58c922bd7719156ea9ca5af6bcc581b3744b230f
jeonghi <jpark0902@kookmin.ac.kr>
2023-03-31 15:25:15 +0900

    [Add] 은행 계좌, 은행 지점 엔티티 & Jpa Repository 정의

---
+++ backend/timebank/src/main/kotlin/kookmin/software/capstone2023/timebank/domain/model/BankAccount.kt
@@ -0,0 +1,42 @@
+package kookmin.software.capstone2023.timebank.domain.model
+import jakarta.persistence.*
+import kookmin.software.capstone2023.timebank.domain.model.BankBranch
+import kookmin.software.capstone2023.timebank.domain.model.OwnerType
+import kookmin.software.capstone2023.timebank.domain.model.User
+import java.time.LocalDateTime
+
+@Entity
+@Table(name = "BankAccount")
+data class BankAccount(
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long = 0,
+
+    val accountId: Long,
+
+    var branchId: Long,
+
+    @Enumerated(EnumType.STRING)
+    val ownerType: OwnerType? = null,
+
+    @Column(nullable = false)
+    val accountNumber: String,
+
+    @Column(nullable = true)
+    var password: String,
+
+    @Column(nullable = true)
+    var iv: String,
+
+    @Column(nullable = false)
+    var balance: Int,
+
+    @Column(nullable = false)
+    val createdAt: LocalDateTime = LocalDateTime.now(),
+
+    @Column(nullable = false)
+    var updatedAt: LocalDateTime = LocalDateTime.now(),
+
+    @Column(nullable = true)
+    val deletedAt: LocalDateTime? = null
+)
