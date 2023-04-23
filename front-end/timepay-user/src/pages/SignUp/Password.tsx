f18d2bbcf84162f30c74accd6c5ab6ff0008bbe3
p3chaeyeon <p3chaeyeon@gmail.com>
2023-04-23 20:30:43 +0900

    SignUp axios(#12)

--- front-end/timepay-user/src/pages/SignUp/Password.tsx
+++ front-end/timepay-user/src/pages/SignUp/Password.tsx
@@ -1,43 +1,93 @@
-import { useEffect, useCallback } from 'react';
-import { useNavigate } from 'react-router-dom';
-import { useSetRecoilState } from 'recoil';
-import { headerTitleState } from '../../states/uiState';
-import { PATH } from '../../utils/paths';
+import { useEffect, useCallback, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { useSetRecoilState } from "recoil";
+import { headerTitleState } from "../../states/uiState";
+import axios from "axios";
+import { PATH } from "../../utils/paths";
 
-const Password = () => { 
+async function signByUserData(name: String, phoneNumber: String) {
+  try { 
+    const access_token = window.localStorage.getItem("access_token");
+    await axios.post(
+      PATH.SERVER + "/api/v1/users/register",
+      {
+        provider: "KAKAO",
+        accessToken: access_token,
+        name: name,
+        phoneNumber: phoneNumber,
+      }
+    ).then((res) => {
+      const { data: { accessToken: timepayAccessToken} } = res;
+      window.localStorage.setItem(
+        "timepay_accesstoken",
+        timepayAccessToken
+      );
+    });
+  } catch (e) {
+    console.error(e);
+  }
+}
+
+const SignUp = () => {
   const navigate = useNavigate();
+  let [name, setName] = useState("");
+  let [phoneNumber, setPhoneNumber] = useState("");
 
   const setHeaderTitle = useSetRecoilState(headerTitleState);
-  useEffect(() => { 
+  useEffect(() => {
     setHeaderTitle(null);
   });
+
   const handleOnClickLinkBtn = useCallback(
-    (path: string) => {
-      navigate(path); 
+    (path: string, name: string, phoneNumber: string) => {
+      console.log("name :" + name + ", phonNumber: " + phoneNumber);
+      signByUserData(name, phoneNumber);
+      navigate(path);
     },
-    [navigate],
+    [navigate]
   );
 
-  const onChangeTest = ()=> {
-    console.log()
-  }
+  const onNumberChanged = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { value } = e.target;
+    const onlyNumber = value.replace(/[^0-9]/g, "");
+    console.log(onlyNumber);
+    setPhoneNumber(onlyNumber);
+  };
 
   return (
     <>
-      <div className = "sign-up">
-        <span className='title'>초기 비밀번호를 설정해주세요</span>
+      <div className="sign-up">
+        <span className="title">이름과 핸드폰 번호를 입력해주세요</span>
         <div className="info-box">
-            <label>초기 비밀번호</label>
-            <input type="text" onChange={onChangeTest}/>
-            <label style={{marginTop:"10px"}}>초기 비밀번호 확인</label>
-            <input type="text" onChange={onChangeTest} />
+          <label>이름</label>
+          <input
+            type="text"
+            onChange={(e) => {
+              setName(e.target.value);
+            }}
+          />
+          <label style={{ marginTop: "10px" }}>핸드폰 번호</label>
+          <input
+            type="text"
+            value={phoneNumber}
+            maxLength={11}
+            onChange={(e) => {
+              onNumberChanged(e);
+            }}
+          />
         </div>
-        <div className='finish-btn' onClick = { ()=>handleOnClickLinkBtn(PATH.MAIN) }>
-            <button>가입하기</button>
+        <div
+          className="finish-btn"
+          onClick={() => handleOnClickLinkBtn(PATH.PASSWORD, name, phoneNumber)}
+        >
+          <button>가입하기</button>
         </div>
+        {/* <div className='finish-btn' onClick = { ()=>handleOnClickLinkBtn(PATH.SIGN_UP) }>
+            <img src={finishBtn} alt=""/>
+        </div> */}
       </div>
     </>
   );
 };
 
-export default Password;
+export default SignUp;
