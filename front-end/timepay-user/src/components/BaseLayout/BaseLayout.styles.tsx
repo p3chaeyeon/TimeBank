60c890ba1243beb83de397b445ee1bccf88360ca
p3chaeyeon <p3chaeyeon@gmail.com>
2023-04-29 18:34:32 +0900

    #41 유저 메인 페이지 디자

--- front-end/timepay-user/src/components/BaseLayout/BaseLayout.styles.tsx
+++ front-end/timepay-user/src/components/BaseLayout/BaseLayout.styles.tsx
@@ -2,6 +2,7 @@ import { css } from '@emotion/react';
 import { COMMON_COLOR } from '../../styles/constants/colors';
 import '../../styles/fonts/pretendard/pretendard.css';
 
+
 export const cssBaseLayoutStyle = css`
   font-family: unset !important;
   background: ${COMMON_COLOR.WHITE};
