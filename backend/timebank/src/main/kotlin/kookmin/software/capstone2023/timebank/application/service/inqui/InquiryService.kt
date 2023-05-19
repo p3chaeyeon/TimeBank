57bea8c32c61928545ac9840914940b5f5668f2e
BbingDdang <39540588+BbingDdang@users.noreply.github.com>
2023-05-19 14:14:49 +0900

    fix usercontext

--- backend/timebank/src/main/kotlin/kookmin/software/capstone2023/timebank/application/service/inqui/InquiryService.kt
+++ backend/timebank/src/main/kotlin/kookmin/software/capstone2023/timebank/application/service/inqui/InquiryService.kt
@@ -67,9 +67,9 @@ class InquiryService(
      */
     @Transactional
 
-    fun createInquiry(request: InquiryCreateRequest, userContext: UserContext): InquiryDto {
-        val user = userJpaRepository.findByIdOrNull(userContext.userId)
-            ?: throw NotFoundException(message = "\"User not found with id: ${userContext.userId}\"")
+    fun createInquiry(request: InquiryCreateRequest, userId: Long): InquiryDto {
+        val user = userJpaRepository.findByIdOrNull(userId)
+            ?: throw NotFoundException(message = "\"User not found with id: ${userId}\"")
         val inquiry = Inquiry(
             title = request.title,
             content = request.content,
